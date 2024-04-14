ba39e950792159673b3fc58a5448b89753083a52
jakobniinja <dansaren124@outlook.com>
2024-04-14 09:49:05 +0200

    Impl Hand of Ragnaros and Backstage, no coverage checking

--- src/main/java/com/jakobniinja/GildedRose.java
+++ src/main/java/com/jakobniinja/GildedRose.java
@@ -10,4 +10,21 @@ public class GildedRose {
   public GildedRose(Item[] items) {
     this.items = items;
   }
+
+  public void updateQuality() {
+    for (Item item : items) {
+      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        item.sellIn -= 1;
+      }
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+
+        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (item.sellIn < 11 && item.quality < 50) {
+            item.quality += 1;
+          }
+        }
+      }
+    }
+  }
 }
--- src/test/java/com/jakobniinja/GildedRoseTest.java
+++ src/test/java/com/jakobniinja/GildedRoseTest.java
@@ -26,4 +26,35 @@ class GildedRoseTest {
     GildedRose gildedRose = new GildedRose(new Item[]{});
     assertEquals(0, gildedRose.items.length);
   }
+
+  @Test
+  void onUpdateQualityNotSulfuras() {
+
+    Item item = new Item("Rune Sword", 1999, 4);
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+    gildedRose.updateQuality();
+    assertEquals(1, gildedRose.items.length);
+    assertEquals(1998, item.sellIn);
+  }
+
+  @Test
+  void onUpdateQualityLessThan50() {
+    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 4);
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+    gildedRose.updateQuality();
+    assertEquals(1, gildedRose.items.length);
+    assertEquals(5, item.quality);
+  }
+
+  @Test
+  void onUpdateQualityLessThan50SellIn11() {
+    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 4);
+    GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+    gildedRose.updateQuality();
+    assertEquals(1, gildedRose.items.length);
+    assertEquals(6, item.quality);
+  }
 }
