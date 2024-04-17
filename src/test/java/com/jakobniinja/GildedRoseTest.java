7d1709745dee10287774afc652f8b73ac10051c4
jakobniinja <dansaren124@outlook.com>
2024-04-17 06:23:45 +0200

    Remove unused test and introduce assertItemQuality

--- src/test/java/com/jakobniinja/GildedRoseTest.java
+++ src/test/java/com/jakobniinja/GildedRoseTest.java
@@ -7,104 +7,73 @@ import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
+  private GildedRose gildedRose;
+
+  private Item item;
+
   @Test
   void testInit() {
     assertNotNull(new GildedRose());
   }
 
   @Test
-  void testItemSize1() {
-    Item item = new Item("Rune Sword", 1999, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
-    assertEquals(1, gildedRose.items.length);
-  }
-
-  @Test
-  void testZeroItems() {
-    GildedRose gildedRose = new GildedRose(new Item[]{});
-    assertEquals(0, gildedRose.items.length);
+  void testRuneQualityLessThan0() {
+    item = new Item("Rune Sword", 1999, -1);
+    gildedRose = new GildedRose(new Item[]{item});
+    gildedRose.updateQuality();
+    assertItemQuality(-1);
   }
 
   @Test
   void testRagnaros() {
-    Item item = new Item("Sulfuras, Hand of Ragnaros", 1999, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
+    item = new Item("Sulfuras, Hand of Ragnaros", 1999, 4);
+    gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
 
     assertEquals("Sulfuras, Hand of Ragnaros", item.name);
-    assertEquals(3, item.quality);
-  }
-
-  @Test
-  void testUpdateQualityNotSulfuras() {
-    Item item = new Item("Rune Sword", 1999, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
-
-    gildedRose.updateQuality();
-    assertEquals(1, gildedRose.items.length);
-    assertEquals(1999, item.sellIn);
-  }
-
-  @Test
-  void testGreaterThan50() {
-    Item item = new Item("Sulfuras, Hand of Ragnaros", 12, 66);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
-
-    gildedRose.updateQuality();
-    assertEquals(1, gildedRose.items.length);
-    assertEquals(65, item.quality);
+    assertItemQuality(3);
   }
 
   @Test
   void testLessThan50() {
-    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
+    item = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 4);
+    gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
-    assertEquals(1, gildedRose.items.length);
-    assertEquals(5, item.quality);
+    assertItemQuality(5);
   }
 
-  @Test
-  void testLessThan11() {
-    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
-
-    gildedRose.updateQuality();
-    assertEquals(1, gildedRose.items.length);
-    assertEquals(6, item.quality);
-  }
 
   @Test
   void testLessThan6() {
-    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 2, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
+    item = new Item("Backstage passes to a TAFKAL80ETC concert", 2, 4);
+    gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
-    assertEquals(1, gildedRose.items.length);
-    assertEquals(7, item.quality);
+    assertItemQuality(7);
   }
 
-
   @Test
-  void testGreaterThan12() {
-    Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 12, 4);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
+  void testAgedBrieLessThan50() {
+    item = new Item("Aged Brie", 12, 1);
+    gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
-    assertEquals(1, gildedRose.items.length);
-    assertEquals(5, item.quality);
+    assertItemQuality(2);
   }
 
-
   @Test
-  void testAgedBrie() {
-    Item item = new Item("Apple", 12, 1);
-    GildedRose gildedRose = new GildedRose(new Item[]{item});
+  void testAgedBrieGreaterThan50() {
+    item = new Item("Aged Brie", 12, 100);
+    gildedRose = new GildedRose(new Item[]{item});
 
     gildedRose.updateQuality();
+    assertItemQuality(100);
+  }
+
+  private void assertItemQuality(int expectedQuality) {
     assertEquals(1, gildedRose.items.length);
-    assertEquals(0, item.quality);
+    assertEquals(expectedQuality, item.quality);
   }
 }
